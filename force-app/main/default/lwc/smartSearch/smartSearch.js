2efbbb8107f1ffbc692f4c8493f669e9504c7b1b
rongkang666 <rongkang666@gmail.com>
2020-01-22 12:28:32 +1030

    Add book smart search functions

--- force-app/main/default/lwc/smartSearch/smartSearch.js
+++ force-app/main/default/lwc/smartSearch/smartSearch.js
@@ -1,33 +1,87 @@
 import { LightningElement, track } from 'lwc';
+import getBooksByTag from '@salesforce/apex/BookController.getBooksByTag';
+import getBooks from '@salesforce/apex/BookController.getBooks';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const columns = [
-    { label: 'Name', fieldName: 'name' },
-    { label: 'Author', fieldName: 'author'},
-    { label: 'Edition', fieldName: 'edition'},
-    { label: 'Summary', fieldName: 'summary'},
-    { label: 'Tag', fieldName: 'tag'},
+    { label: 'Book', fieldName: 'Name' },
+    { label: 'Author', fieldName: 'Author__c'},
+    { label: 'Summary', fieldName: 'Summary__c'},
+    { label: 'ISBN', fieldName: 'ISBN__c'},
+    
 ];
+
+
 
 
 export default class SmartSearch extends LightningElement {
 
   @track inputValue;
+  @track showDataTable = false;
   @track data =[];
   @track columns = columns;
-  @track showResult = false;
+
 
   connectedCallback() {
-      if(this.inputValue != null) {
-          this.showResult = true;
-      }
+    //   if(this.inputValue != null) {
+    //       this.showResult = true;
+    //   }
   }
 
 
 
    handleChange(event) {
        this.inputValue = event.target.value;
-       // eslint-disable-next-line no-console
-       console.log(this.inputValue);
    }
 
+   // eslint-disable-next-line no-unused-vars
+   handleSearchByTag(event) {
+
+       getBooksByTag({keyword: this.inputValue})
+       .then(result => {
+          
+           this.data = [];
+           this.data = result;
+           
+           if(result.length === 0) {
+            this.showError(); 
+           }else{
+             this.showDataTable = true;
+           }
+
+       })
+       .catch(error => {
+           // eslint-disable-next-line no-console
+           console.log(error);
+       })
+   }
+
+   // eslint-disable-next-line no-unused-vars
+   handleSearchByBook(event) {
+     getBooks({keyword: this.inputValue}).
+     then(result => {
+         this.data = [];
+        this.data = result;
+        if(result.length === 0) {
+            this.showError(); 
+           }else{
+             this.showDataTable = true;
+           }
+     })
+     .catch(error => {
+         // eslint-disable-next-line no-console
+         console.log(error);
+     })
+   }
+
+   showError() {
+        
+        this.dispatchEvent(new ShowToastEvent({
+        title: 'No Result',
+        message: 'Nothing Found!',
+        variant: 'error',
+        mode: 'dismissable'
+       }));
+   
+    }
 }
